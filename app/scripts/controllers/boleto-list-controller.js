4e9be312529218830352ecc8492d108144d8cd39
Richar León <mricharleon@gmail.com>
2020-03-29 17:15:14 -0500

    Elimina boleto, modifica metodos de petición al API (#3)

--- app/scripts/controllers/boleto-list-controller.js
+++ app/scripts/controllers/boleto-list-controller.js
@@ -11,14 +11,25 @@ angular
   .module('appApp')
   .controller('boletoListCtrl', boletoListCtrl);
 
-  function boletoListCtrl(boletoService) {
+  function boletoListCtrl($location,
+                          boletoService) {
 
     var boletoListVm = this;
+    boletoListVm.eliminarBoleto = eliminarBoleto;
     init();
 
     function init() {
       boletoService.getBoletos(2).then(function(results){
         boletoListVm.boletos = results;
+      });
+    }
+
+    function eliminarBoleto(boletoId){
+      boletoService.delBoleto(boletoId).then(function (resp) {
+        $location.url("/mis-boletos/");
+      },
+      function error(resp) {
+        console.log(resp);
       });
     }
 
